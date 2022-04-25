fccfec92c942c81647f7098d42a060a7f69b203f
Sanivu86 <sanna19170@student.hamk.fi>
2022-04-25 22:59:22 +0300

    Deleted sample tests

--- cypress/integration/2-advanced-examples/home_page_spec.js
+++ cypress/integration/2-advanced-examples/home_page_spec.js
@@ -1,7 +1,6 @@
 describe('The Home Page', () => {
     it('successfully loads', () => {
-    //cy.exec('npm start') //cmd command to start the app, works but times out. Don't know why...
       cy.visit('/') // baseUrl added in Cypress.json
+      cy.contains("Sanna")
     })
   })
-  //testi
