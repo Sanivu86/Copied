fcea83c1e8e2428584b2cf7b1734523b7c59a44b
Sanivu86 <sanna19170@student.hamk.fi>
2022-04-25 20:29:25 +0300

    Backend test

--- src/Sanna.js
+++ src/Sanna.js
@@ -1,9 +1,34 @@
 function Sanna() {
-    var list = ["Sanna", "Adrien", "Alex"];
-    const a = list.map(elem => <p>I'm {elem} </p>);
-    return (
-     a[0]
-    );
+    // var list = ["Sanna", "Adrien", "Alex"];
+    // const a = list.map(elem => <p>I'm {elem} </p>);
+    // return (
+    //  a[0]
+    // );
+
+    const [backendData, setBackendData] = useState([{}])
+
+useEffect(() => {
+  fetch("/api").then(
+    response => response.json()
+  ).then(
+    data => {
+      setBackendData(data)
+    }
+  )
+}, [])
+  return (
+    <div>
+      <h1> This project is made by:</h1>
+
+      {(typeof backendData.users == 'undefined') ? (
+        <p> Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+    </div>
+  )
   }
   
   export default Sanna;
